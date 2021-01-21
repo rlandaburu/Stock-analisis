16dcd7fcccef2dbbe75c5b90ebadd622f7b143d3
rlandaburu <52452298+rlandaburu@users.noreply.github.com>
2021-01-21 11:33:29 -0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -23,3 +23,20 @@ st.write("""
 ## Volume Price
 """)
 st.line_chart(tickerDf.Volume)
+
+m = folium.Map(width=340,height=580,location=[lat, lon], tiles='cartodbpositron', zoom_start=8)
+import datapane as dp
+from datetime import date
+dp.login(token="your token from datapane")
+dp.Report(
+                f"""
+            _Analysis built {date.today()}_
+                """,
+                dp.Group(
+                    dp.Plot(m, caption='Geographical interest'),
+                    columns=1
+                ),
+            ).publish(name="map3", open=True)
+
+
+
